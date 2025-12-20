0bc1c588e38126a2900b8caab2ed9dbdef23d4a6
Mikhail Kopylov <mih.kopylov@yandex.ru>
2025-12-20 22:14:14 +0300

    Implement self-update

--- internal/tgbot/tgbot.go
+++ internal/tgbot/tgbot.go
@@ -1,19 +1,26 @@
 package tgbot
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"game-controller-bot/internal/botcommand"
 	"game-controller-bot/internal/botcontext"
 	"log"
 	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+	"syscall"
 	"time"
 
+	"github.com/creativeprojects/go-selfupdate"
 	"gopkg.in/telebot.v4"
 )
 
 var ErrWrongBotToken = errors.New("wrong telegram bot token")
 var ErrFailedToSetCommands = errors.New("failed to set up bot commands")
+var ErrFailedToSelfUpdate = errors.New("failed to self update bot")
 
 type TgBot struct {
 	bot      *telebot.Bot
@@ -70,6 +77,73 @@ func (b *TgBot) Start() error {
 		return fmt.Errorf("%w: %w", ErrFailedToSetCommands, err)
 	}
 
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		b.bot.Stop()
+	}()
+
 	b.bot.Start()
+	return nil
+}
+
+func (b *TgBot) StartSelfUpdate(version string) {
+	if version == "0.0.0" {
+		log.Println("Skip self update for developer version")
+		return
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Minute)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				err := b.selfUpdate(version)
+				if err != nil {
+					log.Println(fmt.Errorf("%w: %w", ErrFailedToSelfUpdate, err))
+				}
+			case <-quit:
+				return
+			}
+		}
+	}()
+}
+
+func (b *TgBot) selfUpdate(version string) error {
+	latest, found, err := selfupdate.DetectLatest(
+		context.Background(), selfupdate.ParseSlug("mih-kopylov/game-controller-bot"),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to detect latest version: %w", err)
+	}
+	if !found {
+		return fmt.Errorf("didn't found latest version for %s/%s", runtime.GOOS, runtime.GOARCH)
+	}
+
+	if latest.LessOrEqual(version) {
+		log.Printf("Current version %s is the latest\n", version)
+		return nil
+	}
+
+	exe, err := selfupdate.ExecutablePath()
+	if err != nil {
+		return fmt.Errorf("could not locate executable path: %w", err)
+	}
+	err = selfupdate.UpdateTo(context.Background(), latest.AssetURL, latest.AssetName, exe)
+	if err != nil {
+		return fmt.Errorf("error occurred while updating binary: %w", err)
+	}
+	cmd := exec.Command(os.Args[0], os.Args[1:]...)
+	err = cmd.Start()
+	if err != nil {
+		return fmt.Errorf("failed to start new binary version: %w", err)
+	}
+	log.Printf("Successfully updated to version %s, so exit\n", latest.Version())
+	os.Exit(0)
 	return nil
 }
