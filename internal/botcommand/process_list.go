e0ebbf0f64a7244aefefb11a9b234ea2789f884b
Mikhail Kopylov <mih.kopylov@yandex.ru>
2025-12-21 13:17:39 +0300

    #2 Reduce cyclo complexity

--- internal/botcommand/process_list.go
+++ internal/botcommand/process_list.go
@@ -40,50 +40,11 @@ func (c *ProcessListCommand) GetHandleFunc() telebot.HandlerFunc {
 			grep = strings.ToLower(context.Args()[0])
 		}
 
-		processes, err := process.Processes()
+		allProcesses, err := c.readAllProcesses(data, grep)
 		if err != nil {
 			return err
 		}
-		var allProcesses []ProcessInfo
-		for _, p := range processes {
-			processName, _ := p.Name()
-			processExe, _ := p.Exe()
-			processUsername, _ := p.Username()
-			terminal, _ := p.Terminal()
-			parentPid, _ := p.Ppid()
-			if len(data.NamesToKill) > 0 && !slices.Contains(data.NamesToKill, strings.ToLower(processName)) {
-				continue
-			}
-			if data.SystemUser != "" && processUsername != data.SystemUser {
-				continue
-			}
-			if grep != "" && !strings.Contains(strings.ToLower(processName), grep) {
-				continue
-			}
 
-			allProcesses = append(
-				allProcesses, ProcessInfo{
-					Pid:       p.Pid,
-					Name:      processName,
-					User:      processUsername,
-					Exe:       processExe,
-					ParentPid: parentPid,
-					Terminal:  terminal,
-				},
-			)
-		}
-
-		slices.SortFunc(
-			allProcesses, func(a, b ProcessInfo) int {
-				if a.User != b.User {
-					return strings.Compare(a.User, b.User)
-				}
-				if a.Name != b.Name {
-					return strings.Compare(a.Name, b.Name)
-				}
-				return 0
-			},
-		)
 		message := strings.Builder{}
 		message.WriteString("Список всех процессов, подходящих под фильтр:\n")
 		for _, p := range allProcesses {
@@ -99,6 +60,55 @@ func (c *ProcessListCommand) GetHandleFunc() telebot.HandlerFunc {
 		}
 		return context.Send(message.String())
 	}
+}
+
+func (c *ProcessListCommand) readAllProcesses(data *botcontext.Data, grep string) ([]ProcessInfo, error) {
+	processes, err := process.Processes()
+	if err != nil {
+		return nil, err
+	}
+
+	var allProcesses []ProcessInfo
+	for _, p := range processes {
+		processName, _ := p.Name()
+		processExe, _ := p.Exe()
+		processUsername, _ := p.Username()
+		terminal, _ := p.Terminal()
+		parentPid, _ := p.Ppid()
+		if len(data.NamesToKill) > 0 && !slices.Contains(data.NamesToKill, strings.ToLower(processName)) {
+			continue
+		}
+		if data.SystemUser != "" && processUsername != data.SystemUser {
+			continue
+		}
+		if grep != "" && !strings.Contains(strings.ToLower(processName), grep) {
+			continue
+		}
+
+		allProcesses = append(
+			allProcesses, ProcessInfo{
+				Pid:       p.Pid,
+				Name:      processName,
+				User:      processUsername,
+				Exe:       processExe,
+				ParentPid: parentPid,
+				Terminal:  terminal,
+			},
+		)
+	}
+
+	slices.SortFunc(
+		allProcesses, func(a, b ProcessInfo) int {
+			if a.User != b.User {
+				return strings.Compare(a.User, b.User)
+			}
+			if a.Name != b.Name {
+				return strings.Compare(a.Name, b.Name)
+			}
+			return 0
+		},
+	)
+	return allProcesses, nil
 }
 
 type ProcessInfo struct {
